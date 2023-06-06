562ce2f11a1d6b81b6249f9ddf24ac71745b851e
Allan Martinez <martinezallan123@gmail.com>
2023-06-06 13:09:21 -0700

    allow using picsum.photos with Next's <Image/>

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/320/300",
+      },
+    ],
+  },
+};
+// src = "https://picsum.photos/320/300";
