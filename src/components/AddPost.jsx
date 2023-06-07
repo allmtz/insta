3d34e46c2607d76833ca23044241655ed9265fe4
Allan <martinezallan123@gmail.com>
2023-06-06 20:51:32 -0700

    styling <AddPost>

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { mockUserState } from "../mockData";
+import backArrow from "../assets/icons/back.svg";
 
 export const AddPost = () => {
   return (
-    <div className="flex flex-col rounded-md border-2">
-      <header className="flex justify-between border-b-2">
-        <h2>Create new post</h2>
-        <button>Share</button>
+    <div className="flex flex-col rounded-md bg-white">
+      <header className="flex items-center justify-between border-b-2 p-2">
+        <Image src={backArrow} height={30}></Image>
+        <h2 className=" font-semibold">Create new post</h2>
+        <button className="text-sm font-semibold text-blue-400">Share</button>
       </header>
       <div className="flex">
         <Image
@@ -16,7 +18,7 @@ export const AddPost = () => {
           alt=""
         ></Image>
         <section className="POST-INFO">
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 p-2 ">
             <Image
               src={mockUserState.profilePicSrc}
               className="PROFILE-PIC h-10 w-10 rounded-full object-cover object-top"
@@ -26,13 +28,13 @@ export const AddPost = () => {
             ></Image>
             <p className="font-semibold">{mockUserState.name}</p>
           </div>
-          <input
-            className="min-h-[200px] min-w-full outline-none"
+          <textarea
+            className="mt-2 min-h-[200px] min-w-full  resize-none border-b-2 pl-2 outline-none placeholder:text-gray-300"
             type="text"
             placeholder="Write a caption..."
           />
           <input
-            className="outline-none"
+            className="pl-2 outline-none"
             type="text"
             placeholder="Add location"
           />
