180de0f0283ac70d2e07946df6a784b303135d76
Allan Martinez <martinezallan123@gmail.com>
2023-06-15 10:05:19 -0700

    fix ui bug

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 export const Modal = ({ children, onClick }) => {
   return (
     <div
-      className="MODAL fixed right-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-70"
+      className="MODAL fixed right-0 top-0 z-10 flex h-full w-full items-center justify-center bg-black bg-opacity-70"
       onClick={onClick}
     >
       {children}
