1a02f522b0f7f5e57b60d4ff94aac7c7315b84ce
Allan Martinez <martinezallan123@gmail.com>
2023-06-19 17:39:18 -0700

    making all the links work

--- src/components/PostFocused.tsx
+++ src/components/PostFocused.tsx
@@ -12,34 +12,63 @@ import { interactionsSelector } from "../features/PostInteractions/interactionsS
 import { nanoid } from "@reduxjs/toolkit";
 import { ScrollArea } from "./shadcn/scroll-area";
 import { Post } from "../tipos/types";
+import Link from "next/link";
+import { LikedBy } from "./LikedBy";
+import { useState } from "react";
 
 export const PostFocused = ({ post }: { post: Post }) => {
+  const [showLikedBy, setShowLikedBy] = useState(false);
+
   const allInteractions = useSelector(interactionsSelector);
   const postInteractions = allInteractions[post.id];
 
-  const comments = postInteractions.comments;
+  const profileLink = `/profile/${post.author.uuid}`;
 
+  const comments = postInteractions.comments;
   const commentsDisplayed = comments.map((comment) => (
     <div key={nanoid()} className="items-top mt-2 flex gap-3 text-sm">
-      <ProfilePic picSrc={comment.authorProfilePicSrc} size={"small"} />
+      <Link href={`/profile/${comment.authorID}`}>
+        <ProfilePic picSrc={comment.authorProfilePicSrc} size={"small"} />
+      </Link>
       <p className="max-w-[320px]">
-        <span className="font-bold">{comment.authorHandle}</span>{" "}
+        <Link href={`/profile/${comment.authorID}`}>
+          <span className="font-bold">{comment.authorHandle}</span>
+        </Link>{" "}
         <span className="break-all">{comment.text}</span>
       </p>
     </div>
   ));
+
+  const handleLikesClick = () => {
+    setShowLikedBy(true);
+  };
+
+  const handleModalClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+
+    const { target } = e;
+
+    if (target instanceof HTMLElement && target.classList.contains("MODAL")) {
+      setShowLikedBy(false);
+    }
+  };
 
   return (
     <>
       <div className="flex min-w-max rounded-br-md rounded-tr-md bg-white">
         <Image src={post.imgSrc} height={500} width={500} alt="" />
         <div>
-          <header className="flex items-center gap-3 border-b p-3 text-sm">
-            <ProfilePic picSrc={post.author.profilePicSrc} size="small" />
-            <div>
-              <p className="font-bold">{post.author.handle}</p>
-              <p>{post.location}</p>
-            </div>
+          <div className="flex items-center border-b p-3 text-sm">
+            <Link
+              href={`/profile/${post.author.uuid}`}
+              className="flex items-center gap-3"
+            >
+              <ProfilePic picSrc={post.author.profilePicSrc} size="small" />
+              <div>
+                <p className="font-bold">{post.author.handle}</p>
+                <p>{post.location}</p>
+              </div>
+            </Link>
             <Image
               className="ml-auto"
               src={hdots}
@@ -47,20 +76,28 @@ export const PostFocused = ({ post }: { post: Post }) => {
               width={25}
               alt="options"
             />
-          </header>
+          </div>
+
           <section className="CONTENT flex min-w-[300px] max-w-lg flex-col gap-2 p-3 text-sm">
-            <ScrollArea className=" h-[200px] w-[400px]">
+            <ScrollArea className=" h-[200px] w-[400px] border-b pb-2">
               <div className="CAPTION flex gap-3">
-                <ProfilePic picSrc={post.author.profilePicSrc} size={"small"} />
+                <Link href={profileLink}>
+                  <ProfilePic
+                    picSrc={post.author.profilePicSrc}
+                    size={"small"}
+                  />
+                </Link>
                 <p className="max-w-[320px]">
-                  <span className="font-bold">{post.author.handle}</span>{" "}
+                  <Link href={profileLink} className="font-bold">
+                    {post.author.handle}
+                  </Link>{" "}
                   <span className="break-all">{post.caption}</span>
                 </p>
               </div>
               {commentsDisplayed}
             </ScrollArea>
 
-            <div className="INTERACTIONS mt-2 flex items-center gap-4 border-t pt-2">
+            <div className="INTERACTIONS HERE mt-2 flex items-center gap-4">
               <HeartIcon post={post} />
 
               <label htmlFor="comment">
@@ -77,7 +114,9 @@ export const PostFocused = ({ post }: { post: Post }) => {
               </div>
             </div>
             <div className="POST-INFO border-b pb-2">
-              <p className="font-bold">{postInteractions.likes} likes</p>
+              <p className="font-bold" onClick={() => handleLikesClick()}>
+                {postInteractions.likes} likes
+              </p>
               <p>{moment(post.timestamp).fromNow()}</p>
             </div>
             <div className="COMMENT h-[80px] ">
@@ -86,6 +125,15 @@ export const PostFocused = ({ post }: { post: Post }) => {
           </section>
         </div>
       </div>
+
+      {showLikedBy && (
+        <div
+          className="MODAL fixed right-0 top-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-70"
+          onClick={(e) => handleModalClick(e)}
+        >
+          <LikedBy post={post} />
+        </div>
+      )}
     </>
   );
 };
