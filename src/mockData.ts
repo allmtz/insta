0bd465af7038f375acabb274b9b1c587b94cc9bf
Allan Martinez <martinezallan123@gmail.com>
2023-07-24 17:12:24 -0700

    delete info now stored in `interactions` slice

--- src/mockData.ts
+++ src/mockData.ts
@@ -3,7 +3,6 @@ import johnPic from "./assets/john.jpg";
 import { Post, User } from "./tipos/types";
 import img from "./assets/nature.jpg";
 import pier from "./assets/pier.jpg";
-import { nanoid } from "@reduxjs/toolkit";
 
 const mike: User = {
   uuid: "20",
@@ -27,8 +26,8 @@ const john: User = {
   postsLiked: {},
   postsBookmarked: {},
   posts: [],
-  followers: [mike],
-  following: [mike],
+  followers: [],
+  following: [],
 };
 
 export const johnsPost: Post = {
@@ -36,27 +35,6 @@ export const johnsPost: Post = {
   id: "johnsPost",
   timestamp: "2023-06-13T06:14:00+0000",
   location: "Oceanside, California",
-  likes: 2,
-  likedBy: { [john.handle]: john, [mike.handle]: mike },
-  comments: [
-    {
-      authorHandle: mike.handle,
-      authorID: mike.uuid,
-      id: nanoid(),
-      likes: 0,
-      text: "so sick!",
-      authorProfilePicSrc: mike.profilePicSrc,
-    },
-    {
-      authorHandle: john.handle,
-      authorID: john.uuid,
-      id: nanoid(),
-      likes: 0,
-      text: "thanks mike",
-      authorProfilePicSrc: john.profilePicSrc,
-    },
-  ],
-  commentCount: 2,
   caption: "try Hodad's",
   imgSrc: pier,
 };
@@ -66,19 +44,6 @@ export const mikesPost: Post = {
   id: "mikesPost",
   timestamp: "2023-06-09T04:00:00.977Z",
   location: "San Francisco, California",
-  likes: 2,
-  likedBy: { [john.handle]: john, [mike.handle]: mike },
-  comments: [
-    {
-      authorHandle: john.handle,
-      authorID: john.uuid,
-      id: nanoid(),
-      likes: 0,
-      text: "love this",
-      authorProfilePicSrc: john.profilePicSrc,
-    },
-  ],
-  commentCount: 1,
   caption:
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit rerum laudantium dolore.",
   imgSrc: img,
